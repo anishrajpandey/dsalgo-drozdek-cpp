5e539c4898c91ffd85152ef9c12c29acd7086ddf
Anish Raj Pandey <anishpandey021@gmail.com>
2025-07-07 18:45:57 -0500

    doubly linked list project completed

--- linked_list/browser_tab_manager_doubly_linked_list/Driver.cpp
+++ linked_list/browser_tab_manager_doubly_linked_list/Driver.cpp
@@ -6,15 +6,20 @@ int main()
     Browser br = Browser();
     br.openTab("Facebook", "facebook.com");
     br.openTab("YouTube", "youtube.com");
-    br.openTab("Twitter", "twitter.com");
-    br.openTab("Reddit", "reddit.com");
-    br.openTab("LinkedIn", "linkedin.com");
-    br.openTab("GitHub", "github.com");
-    br.openTab("Google", "google.com");
-    br.openTab("Stack Overflow", "stackoverflow.com");
-    br.openTab("Netflix", "netflix.com");
-    br.openTab("Medium", "medium.com");
+    // br.openTab("Twitter", "twitter.com");
+    // br.openTab("Reddit", "reddit.com");
+    // br.openTab("LinkedIn", "linkedin.com");
+    // br.openTab("GitHub", "github.com");
+    // br.openTab("Google", "google.com");
+    // br.openTab("Stack Overflow", "stackoverflow.com");
+    // br.openTab("Netflix", "netflix.com");
+    // br.openTab("Medium", "medium.com");
 
+    br.movePrev();
     br.showAllTabs();
+    cout << "before closing current tab" << endl;
+    br.showCurrentTab();
+    br.closeCurrentTab();
+    cout << "after closing current tab" << endl;
     br.showCurrentTab();
 }
--- linked_list/browser_tab_manager_doubly_linked_list/TabManager.cpp
+++ linked_list/browser_tab_manager_doubly_linked_list/TabManager.cpp
@@ -5,17 +5,32 @@
 Browser::Browser()
 {
     head = nullptr;
+    current = nullptr;
 }
 Tab::Tab(string url, string title)
 {
     this->title = title;
     this->url = url;
 }
+Browser::~Browser()
+{
+    Tab *p = head;
+    while (p != nullptr)
+    {
+        Tab *temp = p;
+        p = p->next;
+        delete temp;
+    }
+}
 void Browser::openTab(string title, string url)
 {
     Tab *newTab = new Tab(url, title);
     if (isEmpty())
+    {
         head = newTab;
+        current = newTab;
+    }
+
     else
     {
         Tab *p = head;
@@ -30,12 +45,43 @@ void Browser::openTab(string title, string url)
 }
 void Browser::closeCurrentTab()
 {
+    if (isEmpty())
+        return;
+    Tab *temp = current;
+    if (current == head) // current is the first tab
+    {
+
+        head = current = head->next;
+        if (head != nullptr)
+            head->prev = nullptr;
+    }
+
+    else if (current->next == nullptr) // current is the last tab
+    {
+        current->prev->next = nullptr;
+
+        current = temp->prev;
+        temp->prev = nullptr;
+    }
+    else
+    { // current is somewhere in middle
+        current = temp->next;
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+    }
+    delete temp;
 }
 void Browser::moveNext()
 {
+    if (isEmpty() || current->next == nullptr)
+        return;
+    current = current->next;
 }
 void Browser::movePrev()
 {
+    if (isEmpty() || current->prev == nullptr)
+        return;
+    current = current->prev;
 }
 void Browser::showAllTabs()
 {
@@ -51,6 +97,11 @@ void Browser::showAllTabs()
 }
 void Browser::showCurrentTab()
 {
+    if (isEmpty() || current == nullptr)
+    {
+        cout << "No current tab.\n";
+        return;
+    }
     cout << "Displaying Current Tab" << endl;
     cout << current->title << "  " << current->url << endl;
 }
