82f6ef3ffeb90f5cc686d4c5a103f918d7083413
Anish Raj Pandey <anishpandey021@gmail.com>
2025-07-24 21:53:12 -0500

    tree structure

---
+++ binary_trees/intro.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int data)
+    {
+        val = data;
+        left = nullptr;
+        right = nullptr;
+    };
+};
+int main()
+{
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    cout << root->left->right->val;
+}
