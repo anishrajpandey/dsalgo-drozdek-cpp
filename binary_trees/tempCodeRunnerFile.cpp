7b19297f78a1061af321bad40fb07d5f2af3736c
Anish Raj Pandey <anishpandey021@gmail.com>
2025-07-25 22:19:07 -0500

    generating binary treesing

---
+++ binary_trees/tempCodeRunnerFile.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// preorder: root left right
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int value)
+    {
+        val = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+TreeNode *createTree(vector<int> arr, int index)
+{
+    if (index == arr.size())
+        return nullptr;
+    // for(int i=0;i<arr.size();i++){
+    TreeNode *root = new TreeNode(arr[index]);
+    root->left = createTree(arr, index + 1);
+
+    root->right = createTree(arr, index + 2);
+
+    return root;
+
+    // }
+}
+
+int main()
+{
+    vector<int> arr;
+    arr = {3, 9, 20, 15, 7};
+    createTree(arr, 0);
+    return 0;
+}
