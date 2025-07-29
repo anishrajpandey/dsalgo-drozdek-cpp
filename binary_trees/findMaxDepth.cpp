5efe767132150f20fa2ebb9a57f18ea5d304c7d1
Anish Raj Pandey <anishpandey021@gmail.com>
2025-07-29 15:39:41 -0500

    find max depth of a binary tree

---
+++ binary_trees/findMaxDepth.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct Node
+{
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int dig)
+    {
+        val = dig;
+        left = nullptr;
+        right = nullptr;
+    };
+};
+
+int main()
+{
+}
